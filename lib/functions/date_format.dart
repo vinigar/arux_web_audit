f48f2f6d07adc47d5af8d57896b99b82fdee8b70
Alejandro Castillo <alexcastillo@cbluna.com>
2022-08-24 11:50:32 -0700

    Cambio de formato fecha

--- lib/functions/date_format.dart
+++ lib/functions/date_format.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
 String dateFormat(DateTime? x) {
-  String formattedDate = DateFormat('yyyy-MM-dd').format(x!);
+  String formattedDate = DateFormat('dd-MM-yyyy').format(x!);
   return formattedDate;
 }
